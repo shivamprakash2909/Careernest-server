8c400cc1f2704e4a2b69535d01b4a2ea1e397079
Shivam Prakash <shivamprakash444@gmail.com>
2025-08-02 20:39:12 +0530

    x-admin-auth added in index.js cors config

--- middleware/index.js
+++ middleware/index.js
@@ -6,7 +6,7 @@ const corsOptions = {
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "x-requested-with"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-admin-auth"],
 };
 
 // Global middleware setup
